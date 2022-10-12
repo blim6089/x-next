64979257904e18ff7340dc3c39c91f23dc075cce
blim6089 <blim6089@gmail.com>
2022-10-11 20:52:57 -0400

    Completed the Payment page

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,14 +3,40 @@ var modal = document.getElementById("modal");
 initialize();
 
 function initialize() {
-  document.getElementById("tip").addEventListener("click", function (event) {
-    event.preventDefault();
-    modalOpen();
-  });
+  var tip = document.getElementById("tip");
+  if (tip) {
+    tip.addEventListener("click", function (event) {
+      event.preventDefault();
+      modalOpen();
+    });
+  }
 
-  document.getElementById("close").addEventListener("click", function () {
-    modalClose();
-  });
+  var close = document.getElementById("close");
+  if (close) {
+    clos.addEventListener("click", function () {
+      modalClose();
+    });
+  }
+
+  var flagItems = document.getElementsByClassName("flag-item");
+  if (flagItems) {
+    for (var i = 0; i < flagItems.length; i++) {
+      var selectedFlagItem = flagItems[i];
+      selectedFlagItem.addEventListener("click", function () {
+        var id = this.getAttribute("id");
+        var target = this.getAttribute("target");
+        document.getElementById("card-payment-tab").classList.remove("active");
+        document
+          .getElementById("voucher-payment-tab")
+          .classList.remove("active");
+        document.getElementById(id).classList.add("active");
+
+        document.getElementById("card-payment").classList.remove("show");
+        document.getElementById("voucher-payment").classList.remove("show");
+        document.getElementById(target).classList.add("show");
+      });
+    }
+  }
 }
 
 function modalOpen() {
